54a27d796d8a44da60dbeb977f2b494ac9aaee83
thek4n <thek4n@yandex.ru>
2024-09-11 16:44:07 +0300

    fix

--- core/core/urls.py
+++ core/core/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.shortcuts import redirect
 from django.urls import path, include, re_path
 from django.views.static import serve
 from django.views.generic.base import RedirectView
