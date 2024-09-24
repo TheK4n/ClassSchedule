19739c8864c7793677572eb98ad0b4aac285c1fd
thek4n <thek4n@yandex.ru>
2024-09-25 00:23:00 +0300

    change urls and add redirect

--- table/urls.py
+++ table/urls.py
@@ -1,3 +1,4 @@
+from django.shortcuts import redirect
 from django.urls import path, register_converter
 
 from . import views
@@ -8,9 +9,10 @@ register_converter(DateConverter, 'date')
 
 
 urlpatterns = [
-    path("", views.render_groups, name="render_groups"),
+    path("groups", views.render_groups, name="render_groups"),
+    path("", lambda _: redirect("render_groups")),
     path("api/groups", views.get_all_groups, name="get_all_groups"),
-    path("api/<str:group_name>/<date:date>", views.get_table_by_date, name="get_table_by_date"),
-    path("<str:group_name>/today", views.render_today, name="render_today"),
-    path("<str:group_name>/<date:date>", views.render_date, name="render_date"),
+    path("api/groups/<str:group_name>/<date:date>", views.get_table_by_date, name="get_table_by_date"),
+    path("groups/<str:group_name>/today", views.render_today, name="render_today"),
+    path("groups/<str:group_name>/<date:date>", views.render_date, name="render_date"),
 ]
