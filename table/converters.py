9822400ee6142bbd1d708d264363bc869ba107d8
thek4n <thek4n@yandex.ru>
2024-09-26 20:57:29 +0300

    ref converters

--- table/converters.py
+++ table/converters.py
@@ -1,12 +1,12 @@
-from datetime import datetime
+import datetime
 
 
 class DateConverter:
     regex = r'\d{2}-\d{2}-\d{2}'
     format = '%d-%m-%y'
 
-    def to_python(self, value):
-        return datetime.strptime(value, self.format).date()
+    def to_python(self, value: str) -> datetime.date:
+        return datetime.datetime.strptime(value, self.format).date()
 
-    def to_url(self, value):
+    def to_url(self, value: datetime.date) -> str:
         return value.strftime(self.format)
